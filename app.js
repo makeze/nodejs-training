51d726e614eb8aa94f4814f247976229c1839dde
makeze <max.gana93@gmail.com>
2020-07-16 21:19:21 +0200

    yarg tests

--- app.js
+++ app.js
@@ -1,18 +1,56 @@
 const getNotes = require('./notes-app/notes');
+const yargs = require('yargs');
+
 console.log(getNotes());
 
 const command = process.argv[2];
 let output = "User command: ";
 
-switch (command) {
-    case 'add':
-        output += command;
-        break;
-    case 'remove':
-        output += command;
-        break;
-    default:
-        output += "not supported";
-}
+yargs.version('1.0.1');
 
-console.log(output);
+yargs.command({
+    command: 'add',
+    describe: 'Adding a note',
+    builder: {
+        title: {
+            describe: 'Note title',
+            demandOption: true,
+            type: 'string'
+        },
+        body: {
+            describe: 'Note body',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler: function(argv) {
+        console.log('Title: ' + argv.title);
+        console.log('Body: ' + argv.body);
+    }
+});
+
+yargs.command({
+    command: 'remove',
+    describe: 'Removing a note',
+    handler: function() {
+        console.log('Removing a note');
+    }
+});
+
+yargs.command({
+    command: 'read',
+    describe: 'Reading a note',
+    handler: function() {
+        console.log('Reading a note');
+    }
+});
+
+yargs.command({
+    command: 'list',
+    describe: 'Listing a note',
+    handler: function() {
+        console.log('Listing all notes');
+    }
+});
+
+yargs.parse();
