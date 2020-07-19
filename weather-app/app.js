9d36504f1d8c7a7bbe4a49640d5a1c49045ad58d
Maksud <max.gana93@gmail.com>
2020-07-19 02:25:16 +0200

    weather app fix

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,8 +1,8 @@
 const request = require("request");
 
 // wrong api key? check
-const url = "http://api.weatherstack.com/current\n" +
-    "?access_key=79be985d52d12feb7cd3ecbc472740eb\n" +
+const url = "http://api.weatherstack.com/current" +
+    "?access_key=79be985d52d12feb7cd3ecbc472740eb" +
     "&query=New York";
 
 request({url: url}, (error, response) => {
