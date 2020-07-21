d3e80239cb6044fb7417421511978822869de299
makeze <max.gana93@gmail.com>
2020-07-21 18:30:39 +0200

    handlebars fun

--- web-server/app.js
+++ web-server/app.js
@@ -7,7 +7,30 @@ const publicDirectory = path.join(__dirname, 'public');
 console.log(__dirname);
 console.log(path.join(__dirname, '/public'));
 
+app.set('view engine', 'hbs');
 app.use(express.static(publicDirectory));
+
+app.get('', (req, res) => {
+   res.render('index', {
+       title: 'Weather App',
+       name: 'Max Gana'
+   });
+});
+
+app.get('/about', (req, res) => {
+   res.render('about', {
+       title: 'About',
+       name: 'Max Gana'
+   });
+});
+
+
+app.get('/help', (req, res) => {
+    res.render('help', {
+        title: 'Help',
+        helpMessage: 'Here be help page in future'
+    });
+});
 
 app.get('/weather', ((req, res) => {
     res.send(
