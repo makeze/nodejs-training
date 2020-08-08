2ac7937892069c232822771fc53ef32013b06494
Maksud <max.gana93@gmail.com>
2020-08-08 20:40:04 +0200

    async integration task

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -42,6 +42,40 @@ app.get('/users/:id', async (req, res) => {
     }
 });
 
+app.post('/tasks', async (req, res) => {
+    const task = new Task(req.body);
+    try {
+        await task.save();
+        res.status(201).send(task);
+    } catch (e) {
+        res.status(400).send(e);
+    }
+});
+
+
+app.get('/tasks', async (req, res) => {
+    try {
+        const tasks = await Task.find();
+        res.send(tasks);
+    } catch (e) {
+        res.status(500).send(e);
+    }
+});
+
+app.get('/tasks/:id', async (req, res) => {
+    const _id = req.params.id;
+
+    try {
+        const task = await Task.findById(_id);
+        res.send(task);
+        if(!task){
+            res.status(404).send();
+        }
+    } catch (e) {
+        res.status(500).send(e);
+    }
+});
+
 app.listen(port, () => {
     console.log('Server is up on port ' + port)
-})
+});
