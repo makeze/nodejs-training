a5c5e544d0da930630a65e3b3967c398858b3ef4
makeze <max.gana93@gmail.com>
2020-09-11 23:56:38 +0200

    socket stuff

--- chat-app/src/index.js
+++ chat-app/src/index.js
@@ -1,11 +1,19 @@
 const express = require('express');
 const path = require('path');
+const http = require('http');
+const socketio = require('socket.io');
 
 const app = express();
 const publicDirectoryPath = path.join(__dirname, '../public');
+const server = http.createServer(app);
+const io = socketio(server);
 const port = process.env.PORT || 3000;
 
 app.use(express.static(publicDirectoryPath));
+
+io.on('connection', () => {
+    console.log('New connection established');
+});
 
 app.listen(port, () => {
     console.log(`Server is up on port ${port}.`);
