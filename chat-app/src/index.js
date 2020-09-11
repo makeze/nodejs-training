45407e8a96b411aa3941308a1faabe3537bf2c13
makeze <max.gana93@gmail.com>
2020-09-11 23:06:55 +0200

    chat app

---
+++ chat-app/src/index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const publicDirectoryPath = path.join(__dirname, '../public');
+const port = process.env.PORT || 3000;
+
+app.use(express.static(publicDirectoryPath));
+
+app.listen(port, () => {
+    console.log(`Server is up on port ${port}.`);
+});
