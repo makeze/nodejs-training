b25ef12ba79b5ec1fc54f9ba5309f00842d8c3f3
makeze <max.gana93@gmail.com>
2020-09-12 12:08:59 +0200

    fix for socket app

--- chat-app/src/index.js
+++ chat-app/src/index.js
@@ -1,13 +1,14 @@
-const express = require('express');
 const path = require('path');
 const http = require('http');
+const express = require('express');
 const socketio = require('socket.io');
 
 const app = express();
-const publicDirectoryPath = path.join(__dirname, '../public');
 const server = http.createServer(app);
 const io = socketio(server);
+
 const port = process.env.PORT || 3000;
+const publicDirectoryPath = path.join(__dirname, '../public');
 
 app.use(express.static(publicDirectoryPath));
 
@@ -15,6 +16,6 @@ io.on('connection', () => {
     console.log('New connection established');
 });
 
-app.listen(port, () => {
+server.listen(port, () => {
     console.log(`Server is up on port ${port}.`);
 });
