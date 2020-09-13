bead66ed5b8e724ae5d80c5cdc75045f80d7c6fd
makeze <max.gana93@gmail.com>
2020-09-13 20:37:57 +0200

    broadcast socket play

--- chat-app/src/index.js
+++ chat-app/src/index.js
@@ -26,9 +26,14 @@ io.on('connection', (socket) => {
     //     socket.emit('Welcome');
     // });
     socket.emit('message', 'Welcome!');
+    socket.broadcast.emit('message', 'A new user has joined!');
     socket.on('sendMessage', (message) => {
         io.emit('message', message);
         console.log(message);
+    });
+
+    socket.on('disconnect', () => {
+       io.emit('message', 'A user has left!');
     });
 });
 
