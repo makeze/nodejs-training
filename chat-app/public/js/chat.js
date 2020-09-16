558089e5021f3c1473296503010cf082c65182e6
makeze <max.gana93@gmail.com>
2020-09-16 23:37:16 +0200

    message list added

--- chat-app/public/js/chat.js
+++ chat-app/public/js/chat.js
@@ -10,11 +10,28 @@ document.querySelector('#increment').addEventListener('click', () => {
     socket.emit('increment');
 });*/
 
-document.querySelector('#sendMessage').addEventListener('click', () => {
-    let messageText = document.getElementById('messageText').value;
-    document.getElementById('messageText').value = '';
-    console.log(messageText);
+const $messageButton = document.querySelector('#sendMessage');
+const $messageText = document.getElementById('messageText');
+const $location = document.querySelector('#sendLocation');
+const $messages = document.querySelector('#messages');
+
+const messageTemplate = document.querySelector('#messageTemplate').innerHTML;
+
+socket.on('message', (message) => {
+    console.log(message);
+    const html = Mustache.render(messageTemplate, {
+        message
+    });
+    $messages.insertAdjacentHTML('beforeend', html);
+});
+
+$messageButton.addEventListener('click', () => {
+    let messageText = $messageText.value;
+    $messageButton.setAttribute('disabled', 'disabled');
     socket.emit('sendMessage', messageText, (error) => {
+        $messageButton.removeAttribute('disabled');
+        $messageText.value = '';
+        $messageText.focus();
         if(error){
             return console.log(error);
         }
@@ -22,26 +39,22 @@ document.querySelector('#sendMessage').addEventListener('click', () => {
     });
 });
 
-document.querySelector('#sendLocation').addEventListener('click', () => {
+$location.addEventListener('click', () => {
+    $location.setAttribute('disabled', 'disabled');
     if(!navigator.geolocation){
         return alert('Geolocation is not supported by your browser');
     }
     navigator.geolocation.getCurrentPosition((position) => {
-       console.log(position.coords.latitude);
-       console.log(position.coords.longitude);
        let locationData = {
            latitude: position.coords.latitude,
            longitude: position.coords.longitude
        };
        socket.emit('sendLocation', locationData, (error) => {
+           $location.removeAttribute('disabled');
            if(error){
                return console.log(error);
            }
            console.log('Location shared!');
        });
     });
-});
-
-socket.on('message', (message) => {
-   console.log(message);
 });
